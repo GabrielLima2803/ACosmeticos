def5e8c0ea6658b81b95e4eb6e33ab985d5ef587
GabrielLima2803 <gabriellima2803@gmail.com>
2023-06-26 14:51:49 -0300

    Começar a colocar os componentes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>HEADER</header>
+  <aside>MENU</aside>
+  <main>MAIN</main>
+  <footer>FOOTER</footer>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
