b70f4b70504f3a2aa8047c41b804c1504c8a17b1
GabrielLima2803 <gabriellima2803@gmail.com>
2023-07-31 11:41:54 -0300

    Terminar header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { } from 'vue-router'
 
 </script>
 
 <template>
-  <header>HEADER</header>
-  <aside>MENU</aside>
-  <main>MAIN</main>
-  <footer>FOOTER</footer>
-  
-  <RouterView />
+  <header>
+
+    <div class="header">
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top"
+            alt="">
+          Bootstrap
+        </a>
+      </nav>
+    </div>
+
+  </header>
 </template>
 
 <style scoped>
-
+.header {
+  width: 100%;
+  background-color: black;
+}
 </style>
