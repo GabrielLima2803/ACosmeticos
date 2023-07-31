0289b3fbc5d6c96efb84c82756032c6e344a0cd2
GabrielLima2803 <gabriellima2803@gmail.com>
2023-07-31 16:55:41 -0300

    Colocar as imgs

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,140 @@
 <script setup>
-import { } from 'vue-router'
-
+import {} from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-    <div >
-
-      <div class="header">
-   
-      </div>
+  <header class="PBarra">
+    <div>
+      <div class="header"></div>
       <!-- Fim da parte roxa -->
-      <nav class="navbar navbar-light bg-light navBarPrincipal linkRouters">  
+      <nav class="navbar navbar-light bg-light navBarPrincipal linkRouters">
+        <span class="marginRL"> Inicio </span>
 
+        <span class="marginRL"> Marcas </span>
+
+        <span class="marginRL"> Tutoriais </span>
+
+        <span class="marginRL"> Sobre nós </span>
+
+        <span class="marginRL"> Ajuda </span>
+
+        <div>
+          
         
-        <span class="marginRL">
-          Inicio
-        </span>
 
-        <span class="marginRL">
-          Marcas
-        </span>
+        </div>
 
-        <span class="marginRL">
-          Tutoriais
-        </span>
-
-        <span class="marginRL">
-          Sobre nós
-        </span>
-
-        <span class="marginRL">
-          Ajuda
-        </span>
       </nav>
     </div>
-
   </header>
+
+  <main>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis
+    pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis,
+    sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet
+    consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum
+    repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor
+    repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus
+    perferendis pariatur nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam
+    reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum
+    dolor sit amet consectetur adipisicing elit. Consectetur iure accusamus perferendis pariatur
+    nostrum repudiandae, saepe voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae
+    dolor repellendus. Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur
+    adipisicing elit. Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe
+    voluptatum itaque nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus.
+    Necessitatibus vel quas veritatis! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur iure accusamus perferendis pariatur nostrum repudiandae, saepe voluptatum itaque
+    nulla voluptatibus quam reiciendis, sapiente vitae dolor repellendus. Necessitatibus vel quas
+    veritatis!
+  </main>
 </template>
 
 <style scoped>
 .header {
   width: 100%;
-  background-color: #43055D;
-;
+  background-color: #43055d;
   color: white;
   height: 9px;
 }
-.navBarPrincipal{
+
+.PBarra {
+  position: sticky;
+  top: 0px;
+}
+.navBarPrincipal {
   height: 93px;
   box-shadow: 2px 2px 15px 1px rgb(94, 94, 94);
   border: none;
 }
-.linkRouters{
+.linkRouters {
   display: flex;
   justify-content: center;
   align-items: end;
-
 }
-.marginRL{
+.marginRL {
   margin-right: 14px;
   font-size: 18px;
   font-family: 'Jost', sans-serif;
-  
 }
 </style>
