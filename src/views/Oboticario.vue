ae498314e1e6a1a48fe4f439bb9982efa30c8390
kvernert <kverner47@gmail.com>
2023-08-10 11:10:42 -0300

    commit kety

---
+++ src/views/Oboticario.vue
@@ -0,0 +1,48 @@
+<script setup>
+import HeaderOboticario  from './components/header/HeaderOboticario.vue';
+// import FullFooter from './components/footer/FullFooter.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <header-oboticario />
+  </header>
+
+  <div class="container">
+    <main>
+      <!-- Centralizar Logo do Header!!! -->
+      <!-- Centralizar Menu do Footer!!! -->
+     <div class="propagandas">
+      <img src="@/img/propagandas-oboticario/propaganda1.png" alt="">
+      <img src="@/img/propagandas-oboticario/propaganda2.png" alt="">
+      <img src="@/img/propagandas-oboticario/propaganda3.png" alt="">
+      <img src="@/img/propagandas-oboticario/propaganda4.png" alt="">
+      <img src="@/img/propagandas-oboticario/propaganda5.png" alt="">
+      <img src="@/img/propagandas-oboticario/propaganda6.png" alt="">
+     </div>
+
+     <div class="perfumaria">
+      <img src="@/img/perfumaria/malbec.png" alt="">
+      <img src="@/img/perfumaria/coffe.png" alt="">
+      <img src="@/img/perfumaria/drbotica.png" alt="">
+      <img src="@/img/perfumaria/egeo.png" alt="">
+      <img src="@/img/perfumaria/lily.png" alt="">
+     </div>
+    </main>
+
+    <!-- <full-footer/> -->
+    
+  </div>
+</template>
+
+
+<style scoped>
+
+.propaganda{
+  background-color: #393939;
+}
+
+
+</style>
