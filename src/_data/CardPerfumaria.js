43a3f2cec7f7a10d2633db5da6165198f9099478
anaclarag <gomes06anaclara@gmail.com>
2023-08-14 16:47:20 -0300

    commit ana

--- src/_data/CardPerfumaria.js
+++ src/_data/CardPerfumaria.js
@@ -1,165 +1,165 @@
 const malbec = [
   {
     id: 1,
-    img: '',
-    hover:'',
-    origin: ''
+    img: 'https://i.ibb.co/mB1F6RL/p-1-malbec.png',
+    hover:'https://i.ibb.co/KFGjTW7/p-1-malbec-1.png',
+    origin: 'https://i.ibb.co/mB1F6RL/p-1-malbec.png'
   },
   {
     id: 2,
-    img: '',
-    hover:'', 
-    original: ''
+    img: 'https://i.ibb.co/mSzjnZV/p-2-malbec.png',
+    hover:'https://i.ibb.co/9NzM17Y/p-2-malbec-1.png', 
+    original: 'https://i.ibb.co/mSzjnZV/p-2-malbec.png'
   },
   {
     id: 3,
-    img: '',
-    hover:'', 
-    original: ''
+    img: 'https://i.ibb.co/y5tbK54/malbec1.jpg',
+    hover:'https://i.ibb.co/sFJY1gW/p-3-malbec.png', 
+    original: 'https://i.ibb.co/y5tbK54/malbec1.jpg'
   },
   {
     id: 4,
-    img: '',
-    hover:'', 
-    original: ''
+    img: 'https://i.ibb.co/t4Rk8gJ/p-4-malbec.png',
+    hover:'https://i.ibb.co/GxtVj1h/p-4-malbec-1.png', 
+    original: 'https://i.ibb.co/t4Rk8gJ/p-4-malbec.png'
   },
   {
     id: 5,
-    img: '',
-    hover:'', 
-    original: ''
+    img: 'https://i.ibb.co/8YnvWTk/p-5-malbec.png',
+    hover:'https://i.ibb.co/71xNgcB/p-5-malbec-1.png', 
+    original: 'https://i.ibb.co/8YnvWTk/p-5-malbec.png'
   },
 ]
 
 const coffe = [
     {
       id: 1,
-      img: '',
-      hover:'',
-      origin: ''
+      img: 'https://i.ibb.co/r2tZ7X0/p-1-coffee.png',
+      hover:'https://i.ibb.co/R45MLGm/p-1-coffee-1.png',
+      origin: 'https://i.ibb.co/r2tZ7X0/p-1-coffee.png'
     },
     {
       id: 2,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/XbGHq9d/p-2-coffee.png',
+      hover:'https://i.ibb.co/jv9drtg/p-2-coffee-1.png', 
+      original: 'https://i.ibb.co/XbGHq9d/p-2-coffee.png'
     },
     {
       id: 3,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/Pm6CxzP/coffee1webp.png',
+      hover:'https://i.ibb.co/Ycq2vrn/p-3-coffee.png', 
+      original: 'https://i.ibb.co/Pm6CxzP/coffee1webp.png'
     },
     {
       id: 4,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/qs9g0tY/p-4-coffee.png',
+      hover:'https://i.ibb.co/6mKCJN9/p-4-coffee-1.png', 
+      original: 'https://i.ibb.co/qs9g0tY/p-4-coffee.png'
     },
     {
       id: 5,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/bzFxXqz/p-5-coffee.png',
+      hover:'https://i.ibb.co/5x3VwdY/p-5-coffee-1.png', 
+      original: 'https://i.ibb.co/bzFxXqz/p-5-coffee.png'
     },
   ]
 
 const drbotica = [
     {
       id: 1,
-      img: '',
-      hover:'',
-      origin: ''
+      img: 'https://i.ibb.co/6HkhKvN/p-1-lily.png',
+      hover:'https://i.ibb.co/rMrpSvZ/p-1-dr-botica.png',
+      origin: 'https://i.ibb.co/6HkhKvN/p-1-lily.png'
     },
     {
       id: 2,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/SBdZMNz/p-2-dr-botica.png',
+      hover:'https://i.ibb.co/B2sqZY3/p-2-dr-botica-1.png', 
+      original: 'https://i.ibb.co/SBdZMNz/p-2-dr-botica.png'
     },
     {
       id: 3,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/m5YwPMG/botica1.png',
+      hover:'https://i.ibb.co/g62ZJ8p/p-3-dr-botica.png', 
+      original: 'https://i.ibb.co/m5YwPMG/botica1.png'
     },
     {
       id: 4,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/hVt9RTC/p-4-dr-botica.png',
+      hover:'https://i.ibb.co/Z1Pfmks/p-4-dr-botica-1.png', 
+      original: 'https://i.ibb.co/hVt9RTC/p-4-dr-botica.png'
     },
     {
       id: 5,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/wQFhGm7/p-5-dr-botica.png',
+      hover:'https://i.ibb.co/wJTByz2/p-5-dr-botica-1.png', 
+      original: 'https://i.ibb.co/wQFhGm7/p-5-dr-botica.png'
     },
   ]
 
 const egeo = [
     {
       id: 1,
-      img: '',
-      hover:'',
-      origin: ''
+      img: 'https://i.ibb.co/PWkyPyw/p-1-egeo.png',
+      hover:'https://i.ibb.co/TY7Rpxs/p-1-egeo-1.png',
+      origin: 'https://i.ibb.co/PWkyPyw/p-1-egeo.png'
     },
     {
       id: 2,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/5cgpswS/p-2-egeo.png',
+      hover:'https://i.ibb.co/jh9p6m5/p-2-egeo-1.png', 
+      original: 'https://i.ibb.co/5cgpswS/p-2-egeo.png'
     },
     {
       id: 3,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/D4pwjQ1/egeo1.png',
+      hover:'https://i.ibb.co/LtmXR5x/p-3-egeo.png', 
+      original: 'https://i.ibb.co/D4pwjQ1/egeo1.png'
     },
     {
       id: 4,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/x3h43g9/p-4-egeo.png',
+      hover:'https://i.ibb.co/2kk3HgQ/p-4-egeo-1.png', 
+      original: 'https://i.ibb.co/x3h43g9/p-4-egeo.png'
     },
     {
       id: 5,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/q0sCtXY/p-5-egeo.png',
+      hover:'https://i.ibb.co/0DbXg6j/p-5-egeo-1.png', 
+      original: 'https://i.ibb.co/q0sCtXY/p-5-egeo.png'
     },
   ]
 
 const lily = [
     {
       id: 1,
-      img: '',
-      hover:'',
-      origin: ''
+      img: 'https://i.ibb.co/fFZ5xsM/p-1-lily-1.png',
+      hover:'https://i.ibb.co/K2MSkTj/p-1-lily-2.png',
+      origin: 'https://i.ibb.co/fFZ5xsM/p-1-lily-1.png'
     },
     {
       id: 2,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/FVgZtkx/p-2-lily.png',
+      hover:'https://i.ibb.co/m6CVykV/lily-absolute.png', 
+      original: 'https://i.ibb.co/FVgZtkx/p-2-lily.png'
     },
     {
       id: 3,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/LZtYBXq/lyli1.jpg',
+      hover:'https://i.ibb.co/X2fmg0v/p-3-lily.png', 
+      original: 'https://i.ibb.co/LZtYBXq/lyli1.jpg'
     },
     {
       id: 4,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/mvmq9Ls/p-2-lily-1.png',
+      hover:'https://i.ibb.co/1KHm8NQ/p-4-lily.png', 
+      original: 'https://i.ibb.co/mvmq9Ls/p-2-lily-1.png'
     },
     {
       id: 5,
-      img: '',
-      hover:'', 
-      original: ''
+      img: 'https://i.ibb.co/BKGWcyR/image-129.png',
+      hover:'https://i.ibb.co/sFmH30Y/lily2.jpg', 
+      original: 'https://i.ibb.co/BKGWcyR/image-129.png'
     },
   ]
 
