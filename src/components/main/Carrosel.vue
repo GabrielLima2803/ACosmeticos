a8b6261cb75632b406bfb3062d35fcf5d5d6e856
GabrielLima2803 <106837380+GabrielLima2803@users.noreply.github.com>
2023-08-17 20:04:01 -0300

    Imports do django feito

--- src/components/main/Carrosel.vue
+++ src/components/main/Carrosel.vue
@@ -2,33 +2,22 @@
 <template>
     <div class="marginCarrosel">
 
-        <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100"
-                        src="https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:low,w_1220/v1/banner/2023_08_07_19_32_01_9/b23437c6-e20f-4d87-a0c3-0bcbd1e3a9f8-2440x750px-billboard-23c12-kiss-me-more-desk.jpg"
-                        alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100"
-                        src="https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:low,w_1220/v1/banner/2023_08_07_19_32_01_9/b23437c6-e20f-4d87-a0c3-0bcbd1e3a9f8-2440x750px-billboard-23c12-kiss-me-more-desk.jpg"
-                        alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100"
-                        src="https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:low,w_1220/v1/banner/2023_08_07_19_32_01_9/b23437c6-e20f-4d87-a0c3-0bcbd1e3a9f8-2440x750px-billboard-23c12-kiss-me-more-desk.jpg"
-                        alt="Third slide">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only"></span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only"></span>
-            </a>
-        </div>
+<v-carousel>
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
 
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
+// import { createPinia } from 'pinia';
+
 import App from './App.vue'
 import router from './router'
+// import './plugins/axios';
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -21,6 +24,7 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 
+// app.use(createPinia());
 app.use(router)
 app.use(vuetify)
 
