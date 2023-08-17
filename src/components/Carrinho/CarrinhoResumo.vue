697bae3b2b3685162c3c284f08a1a54b2a0f918a
kvernert <kverner47@gmail.com>
2023-08-17 11:09:56 -0300

    Tenho que terminar os cards da perfumaria

--- src/components/Carrinho/CarrinhoResumo.vue
+++ src/components/Carrinho/CarrinhoResumo.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1>Itens do carrinho</h1>
+    <h1>Itens do carrinhos</h1>
 </template>
--- src/components/main/main-cards-principal/PerfumariaCard1.vue
+++ src/components/main/main-cards-principal/PerfumariaCard1.vue
@@ -24,52 +24,39 @@ const perfumaria = [
 </script>
 
 <template>
-  <v-row>
+   <v-row>
     <v-col
       v-for="(produto, index) in perfumaria"
       :key="produto.id"
+      class="col"
     >
-      <v-card class="perfumariaitens">
-        <v-hover v-slot="{ hover }">
-          <img
-            :src="hover ? produto.hover || produto.img : produto.img"
-            :elevation="hover ? 16 : 2"
-            :class="{ 'on-hover': hover, 'enlarge': hover }"
+    <v-hover v-slot="{ hover }"
+      >
+      <img
+            :src="produto.img"
+            :class="{ 'enlarge': hover }"
             class="mx-auto img"
           />
-        </v-hover>
-      </v-card>
+      </v-hover>
     </v-col>
   </v-row>
 </template>
 
 <style scoped>
-/* ... (seu estilo existente) */
-
-/* Adicione uma classe para aumentar o tamanho da imagem no hover */
-.zoom {
-  padding: 50px;
-
-  transition: transform .2s;
-  width: 200px;
-  height: 200px;
-  margin: 0 auto;
+.enlarge {
+  transform: scale(1.25); /* Aumenta o texto em 25% */
+  transition: transform 0.2s;
 }
 
-.zoom:hover {
-  -ms-transform: scale(1.5); /* IE 9 */
-  -webkit-transform: scale(1.5); /* Safari 3-8 */
-  transform: scale(1.5); 
+.col {
+  padding: 0;
 }
-img {
-  width: 222.37px;
+.img{
   height: 255px;
 }
-.perfumariaitens {
-  display: flex;
-  justify-content: space-between; /* Distribui os elementos uniformemente */
-}
-.img {
-  margin: 0;
+/* Defina a largura da imagem */
+img {
+  width: 100%;
+  height: auto;
 }
 </style>
