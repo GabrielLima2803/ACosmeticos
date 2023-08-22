9aa295b3183f1c1e32455ae32c49cf2c838454c0
anaclarag <gomes06anaclara@gmail.com>
2023-08-22 15:06:39 -0300

    atualizado as imagens

--- src/_data/corpoebanho.js
+++ src/_data/corpoebanho.js
@@ -1,22 +1,37 @@
 const nativaSpa = [
     {
       id: 1,
+      nome:'Creme Morango Ruby',
+      descrição:'Loção Desodorante Corporal 400ml',
+      preço:'R$ 74,90',
       img: 'https://i.ibb.co/59tYNb4/p-1-spa-1.png',
     },
     {
       id: 2,
+      nome:'Creme Acquagel Quinoa',
+      descrição:'Loção Desodorante Corporal 250g',
+      preço:'R$ 69,90',
       img: 'https://i.ibb.co/59tYNb4/p-1-spa-1.png'
     },
     {
       id: 3,
+      nome:'Creme Limão e Patchouli',
+      descrição:'Loção Desodorante Corporal 400ml',
+      preço:'R$ 74,90',
       img: 'https://i.ibb.co/ww2bVsp/p-3-spa.png',
     },
     {
       id: 4,
+      nome:'Creme Pitaya',
+      descrição:'Loção Desodorante Corporal 250g',
+      preço:'R$ 59,90',
       img: 'https://i.ibb.co/sgjZ1V6/p-4-spa.png',
     },
     {
       id: 5,
+      nome:'Creme Caviar',
+      descrição:'Loção Desodorante Corporal 400ml',
+      preço:'R$ 74,90',
       img: 'https://i.ibb.co/hWgyXdw/p-5-spa.png',
     },
    
@@ -24,23 +39,38 @@ const nativaSpa = [
 ]
 const cuideSeBem = [
     {
-      id: 1,
+      id: 1, 
+      nome:'Body Splash Biscoito',
+      descrição:'Desodorante Colônia 100ml',
+      preço:'R$ 69,90',                     
       img: 'https://i.ibb.co/vZQbSv4/p-1-cuide-se-bem.png',
     },
     {
       id: 2,
+      nome:'Body Splash Boa noite',
+      descrição:'Desodorante Colônia 100ml',
+      preço:'R$ 69,90',
       img: 'https://i.ibb.co/nsV8Q04/p-2-cuide-se-bem.png'
     },
     {
       id: 3,
+      nome:'Body Splash Nuvem',
+      descrição:'Desodorante Colônia 100ml',
+      preço:'R$ 69,90',
       img: 'https://i.ibb.co/wS3MR76/p-3-cuide-se-bem.png',
     },
     {
       id: 4,
+      nome:'Body Splash Pessegura',
+      descrição:'Desodorante Colônia 100ml',
+      preço:'R$ 69,90',
       img: 'https://i.ibb.co/gMs1Z3n/p-4-cuide-se-bem.png',
     },
     {
       id: 5,
+      nome:'Body Splash Bendito Cacto',
+      descrição:'Desodorante Colônia 100ml',
+      preço:'R$ 69,90',
       img: 'https://i.ibb.co/qFDckRr/p-5-coffee.png',
     },
    
@@ -49,18 +79,30 @@ const cuideSeBem = [
 const liz = [
     {
       id: 1,
+      nome:'Creme Liz Inteso',
+      descrição:'Hidratante Desodorante Corpora 250g',
+      preço:'R$ 59,90',
       img: 'https://i.ibb.co/QQwN14j/p-1-liz.png',
     },
     {
       id: 2,
+      nome:'Creme liz',
+      descrição:'Hidratante Desodorante Corpora 250g',
+      preço:'R$ 59,90',
       img: 'https://i.ibb.co/pwFCqkR/p-2-liz.png'
     },
     {
       id: 3,
+      nome:'',
+      descrição:'',
+      preço:'R$ 33,90',
       img: 'https://i.ibb.co/jyY0NxY/p-3-liz.png',
     },
     {
       id: 4,
+      nome:'',
+      descrição:'Hidratante Desodorante Corporal 200ml',
+      preço:'R$ 33,90',
       img: 'https://i.ibb.co/fvb5cbK/p-4-dr-botica.png',
     },
     {
@@ -73,6 +115,9 @@ const liz = [
 const malbecCorpo = [
     {
       id: 1,
+      nome:'',
+      descrição:'',
+      preço:'',
       img: 'https://i.ibb.co/xLsFbxM/p-1-malbec.png',
     },
     {
@@ -97,6 +142,9 @@ const malbecCorpo = [
 const zaad = [
     {
       id: 1,
+      nome:'',
+      descrição:'',
+      preço:'',
       img: 'https://i.ibb.co/TWn7FVr/p-1-zaad.png',
     },
     {
