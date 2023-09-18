01a9dff0c550ccfb5cbdfcc2f9747b654953f0fc
GabrielLima2803 <gabriellima2803@mail.com>
2023-09-18 15:17:13 -0300

    Correção de bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,26 +6,45 @@ import BlackProg from '@/components/main/BlackProg.vue';
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk1MDYwNzg4LCJpYXQiOjE2OTUwNjA0ODgsImp0aSI6IjBhZjZjMzI5ZWMxNTQ4YmY4YWE2MzYxOWQ3MzQxNTgxIiwidXNlcl9pZCI6MX0.ZvnIP3HhiYSTPorvtHBGMh5DFYCB4M1Xy-w6NlvpEVw'
+
 const marcasAPI = async () => {
   try {
-    const { data } = await axios.get('/marcas/');
+    const { data } = await axios.get('/marcas/', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return data;
   } catch (error) {
     console.error('Erro ao buscar todas as marcas:', error);
+
+    // Se o token expirar, renova o token e tenta novamente a solicitação
+    if (error.response.status === 401) {
+      const newToken = await renovarToken();
+      return marcasAPI({ headers: { Authorization: `Bearer ${newToken}` } });
+    }
+
     throw error;
   }
 };
 
-const marcaApi = marcasAPI();
+const renovarToken = async () => {
+  // Renova o token aqui
+  // ...
+};
 
 const marcas = ref([]);
 
-const buscarmarcas = async () => {
-  marcas.value = await marcaApi;
+const buscarMarcas = async () => {
+  try {
+    marcas.value = await marcasAPI();
+  } catch (error) {
+    console.error('Erro ao buscar todas as marcas:', error);
+  }
 };
 
-onMounted(buscarmarcas);
-
+onMounted(buscarMarcas);
 
 </script>
 <template>
