d441d61e8d7fcc8bcd0748b108aa6df88147a024
GabrielLima2803 <gabreillima2803@gmail.com>
2023-09-18 12:57:42 -0300

    Conexão API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,31 @@
 import FullCard from '@/components/main/FullCard.vue'
 import Carrosel from '@/components/main/Carrosel.vue';
 import BlackProg from '@/components/main/BlackProg.vue';
+
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const marcasAPI = async () => {
+  try {
+    const { data } = await axios.get('/marcas/');
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar todas as marcas:', error);
+    throw error;
+  }
+};
+
+const marcaApi = marcasAPI();
+
+const marcas = ref([]);
+
+const buscarmarcas = async () => {
+  marcas.value = await marcaApi;
+};
+
+onMounted(buscarmarcas);
+
+
 </script>
 <template>
     <div class="z-1">
@@ -9,6 +34,11 @@ import BlackProg from '@/components/main/BlackProg.vue';
     </div>
     <full-card />
     <black-prog/>
+
+    a
+<div v-for="marca in marcas" :key="marca.id">
+    {{ marca.nome_marcas }} 
+</div>
 </template>
 
 <style scoped>
